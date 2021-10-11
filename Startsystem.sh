258d4b5edba905feec5c6d200062c68bc164b982
yxd129220 <52790811+yxd129220@users.noreply.github.com>
2021-10-11 11:34:46 +0800

    Update Startsystem.sh

--- Startsystem.sh
+++ Startsystem.sh
@@ -1,2 +1,19 @@
-!#/bin/bash
-#待完成
+#!/bin/bash
+
+#关闭防火墙
+echo Start to turn off the firewall.
+systemctl stop firewalld
+systemctl disable firewalld
+echo firewall is dead.
+
+#关闭selinux
+echo Start to turn off the selinux.
+sed -i 's/=enforcing/=disabled/' /etc/selinux/config
+setenforce 0
+echo Selinux is off.
+
+#关闭swap
+echo Start to turn off swap.
+swapoff -a
+sed -i 's/^[^#].*swap*/#&/g' /etc/fstab
+echo Swap is off.
